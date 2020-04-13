8ca1fb82c0eb83caa449bc17b7850f494c12fdb7
chialunglee <105502539@cc.ncu.edu.tw>
2020-04-13 11:17:17 +0800

    修正正確率計算方式（如取最短距離、投影 2 次、計算每一點的距離等）、增進程式碼可讀性

--- PCALDA_Test.m
+++ PCALDA_Test.m
@@ -1,5 +1,4 @@
-function [correct] = PCALDA_Test(projectPCA, LDAVector, allGroupMean)
-    principlenum = 50;
+function [correct] = PCALDA_Test(FFACE, projectPCA, finalEigVector)
     % 再次降維成你想要的維度
     ldanum = 50;
     
@@ -7,67 +6,31 @@ function [correct] = PCALDA_Test(projectPCA, LDAVector, allGroupMean)
     withinsample = 5;
     totalcount = 0;
     correct = 0;
-    projectLDA = LDAVector(:, 1:1:ldanum);
+    projectLDA = finalEigVector(:, 1:1:ldanum);
     fileName = ['ORL3232' '\' '1' '\' '2' '.bmp'];
     [imageHeight, imageWidth] = size(imread(fileName));
     % 訓練資料 startFrom == 1, 測試資料 startFrom == 2
     startFrom = 2;
     TestFFACE = readImage(imageHeight, imageWidth, people, withinsample, startFrom);
-    pcaTestTotalFACE = TestFFACE * projectPCA;
+    zeroMeanTestFACE = TestFFACE - mean(FFACE);
+    pcaTestTotalFACE = zeroMeanTestFACE * projectPCA;
+    ldaTestTotalFACE = pcaTestTotalFACE * projectLDA;
+    zeroMeanFACE = FFACE - mean(FFACE);
+    pcaTrainTotalFACE = zeroMeanFACE * projectPCA;
+    ldaTrainTotalFACE = pcaTrainTotalFACE * projectLDA;
     
     % sqrt(sum((A - B).^2));
-    % sqrt(sum((pcaTestTotalFACE(x, :) - allGroupMean(y, :)).^2));
-    fortyDistance = zeros(1, 40);
+    fortyDistance = zeros(1, 200);
     
     for i = 1:1:people * withinsample
         totalcount = totalcount + 1;
-        for j = 1:1:people
-            fortyDistance(j) = sqrt(sum((pcaTestTotalFACE(i, :) - allGroupMean(j, :)).^2));
+        for j = 1:1:people * withinsample
+            fortyDistance(j) = sqrt(sum((ldaTestTotalFACE(i, :) - ldaTrainTotalFACE(j, :)).^2));
         end
-        [~, maxIndex] = max(fortyDistance);
-        if (floor(i / withinsample) + 1 == maxIndex)
+        [~, minIndex] = min(fortyDistance);
+        if (floor((i + 4) / withinsample) == floor((minIndex + 4) / withinsample))
             correct = correct + 1;
         end
     end
     correct = correct / totalcount;
-    
-    
-    
-    
-%     for i = 1:1:people
-%         for j = 2:2:10
-%             theData = pcaTestTotalFACE((i - 1) * withinsample + (j - startFrom) / 2 + 1, :)
-%             for k = 1:1:people
-%                 for m = 1:1:50
-%                     distanceSquare = (allGroupMean(k, m) - theData(m))^2
-            
-    % Testing begin
-%     for i = 1:1:people
-%         for j = 2:2:10
-%             totalcount = totalcount + 1;
-%             tempF = [];
-%             s = ['ORL3232' '\' num2str(i) '\' num2str(j) '.bmp'];
-%             test = imread(s);
-%             [row, col] = size(test);
-%             test = double(test);
-%             for k = 1:1:row
-%                 tempF = [tempF, test(k, :)];
-%             end
-%             tempF = tempF - TotalMeanFACE;
-%             % row
-%             tempF = tempF * projectPCA;
-%             resultF = tempF * projectLDA;
-%         end
-%     end
 end
-%             mindistanceFinal = Inf;
-%             
-%             for k = 1:withinsample:people * withinsample
-%                 mindistanceF = Inf;
-%                 % Nearest neighbor
-%                 for p = k:1:(k - 1 + withinsample)
-%                     OAF = resultF - prototypeFACE(p, :);
-%                     Eucdis = OAF * OAF';
-%                     if (Eucdis < mindistanceF)
-%                         mindistanceF = Eucdis;
-%                     end
--- PCALDA_Train.m
+++ PCALDA_Train.m
@@ -1,55 +1,40 @@
-function [FFACE, PCA, pcaTotalFACE, tempSSW, finalEigVector, finalEigValue, allGroupMean, projectPCA] = PCALDA_Train()
+function [FFACE, pcaEigVector, pcaTotalFACE, tempSSW, finalEigVector, finalEigValue, allGroupMean, projectPCA] = PCALDA_Train()
     % 有 40 個人
     people = 40;
     % 每個樣本取 5
     withinsample = 5;
     % 32 * 32 == 1024 降維到 50
     principlenum = 50;
+    % 取得圖片尺寸
     fileName = ['ORL3232' '\' '1' '\' '1' '.bmp'];
     [imageHeight, imageWidth] = size(imread(fileName));
     % 訓練資料 startFrom == 1, 測試資料 startFrom == 2
     startFrom = 1;
-    % 降維後的訓練樣本
     % FFACE 變成 200 * 1024
     FFACE = readImage(imageHeight, imageWidth, people, withinsample, startFrom);
-    % output 1 * 1024
-    zeromeanTotalFACE = calculateZeroMean(FFACE);
+    % 計算 zeroMean
+    zeromeanTotalFACE = FFACE - mean(FFACE);
 
     % pcaSST = cov(zeromeanTotalFACE);
     % "'" Transpose
     % SST == SSB + SSW
     % 全部變異量等於組間變異量加上組內變異量
-    SST = zeromeanTotalFACE' * zeromeanTotalFACE;
+    pcaCovarianceMatrix = zeromeanTotalFACE' * zeromeanTotalFACE;
     % PCA 是變數名稱紀錄 eigenvector
     % latent 紀錄 eigenvalue
-    [PCA, latent] = eig(SST);
+    [pcaEigVector, latent] = eig(pcaCovarianceMatrix);
     % latent 只有對角線有值，其他都是 0
     % 把 latent 存成一維
     eigvalue = diag(latent);
     % 找大的 eigenvalue
-    % ex. [5, 2, 1, 4] index [1, 2, 3, 4]
-    % junk == [5, 4, 2, 1]
-    % index == [1, 4, 2, 3]
-    [junk, index] = sort(eigvalue, 'descend');
-    % 0.1 0.3 0.3 0.1
-    % 0.2 0.4 0.7 0.8
-    %index 1, 4
-    % PCA
-    % 0.1 0.1
-    % 0.2 0.8
+    [~, index] = sort(eigvalue, 'descend');
     % 對所有 eigenvector 做排序
-    PCA = PCA(:, index);
-    eigvalue = eigvalue(index);
+    pcaEigVector = pcaEigVector(:, index);
 
     % projectPCA 1024 * 50
-    projectPCA = PCA(:, 1:principlenum);
-    pcaTotalFACE = [];
+    projectPCA = pcaEigVector(:, 1:principlenum);
     % 200 筆資料全部被降成 50 維
-    for i = 1:1:withinsample * people
-        tempFACE = zeromeanTotalFACE(i, :);
-        tempFACE = tempFACE * projectPCA;
-        pcaTotalFACE = [pcaTotalFACE; tempFACE];
-    end
+    pcaTotalFACE = zeromeanTotalFACE * projectPCA;
 
     % groupset 5 * 50
     % pcaTotalFACE(1:1:5, :)
@@ -68,21 +53,4 @@ function [FFACE, PCA, pcaTotalFACE, tempSSW, finalEigVector, finalEigValue, allG
     finalCov = inv(tempSSW) * SSB;
 
     [finalEigVector, finalEigValue] = eig(finalCov);
-
-    % 沒 zeromean
-    %{
-    for i=1:withinsample:withinsample*people
-        withinFACE = pcaTotalFACE(i:i + withinsample - 1, :);
-        if (i == 1)
-            SW = withinFACE' * withinFACE;
-            ClassMean = mean(withinFACE);
-        end
-        if (i > 1)
-            SW = SW + withinFACE' * withinFACE;
-            ClassMean = [ClassMean;mean(withinFACE)];
-        end
-    end
-    pcaTotalmean = mean(pcaTotalFACE);
-    SB = ClassMean' * ClassMean;
-    %}
 end
